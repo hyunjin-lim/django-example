5cc927cae6b70a318ed3905278f36e08e9dd252a
hyunjin-lim <limhj9230@gmail.com>
2021-06-15 16:44:01 +0900

    radis exceptions

--- setting/redis_connect.py
+++ setting/redis_connect.py
@@ -1,9 +1,17 @@
 import redis
-from rest_framework import exceptions
 from setting.settings import (REDIS_HOST, REDIS_PORT, REDIS_DB)
 from setting.exceptions import RedisError
 
-try:
-    r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
-except redis.RedisError:
-    raise RedisError()
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
+
+
+def is_redis_available(r):
+    try:
+        r.ping()
+    except (redis.exceptions.ConnectionError):
+        raise RedisError()
+    return True
+
+
+if is_redis_available(r):
+    print("radis connected!")
